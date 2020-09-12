b011fd7e549ffa1c6f41527a5490aefe5ca99bcc
Olajide Emmanuel <57138729+princedevc@users.noreply.github.com>
2020-09-12 08:55:03 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,98 @@
+/* eslint-env node */
+/*
+ * @license
+ * Your First PWA Codelab (https://g.co/codelabs/pwa)
+ * Copyright 2019 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+'use strict';
+
+const express = require('express');
+const fetch = require('node-fetch');
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+
+// CODELAB: Change this to add a delay (ms) before the server responds.
+const FORECAST_DELAY = 5000;
+
+// CODELAB: If running locally, set your Dark Sky API key here
+const API_KEY = "392e928626887a800edc37fd0b877c82";
+const BASE_URL = `http://api.openweathermap.org/data/2.5/forecast?q=`
+.concat(location, "&units=metric&appid=").concat(API_KEY);
+
+// var query = "https://api.openweathermap.org/data/2.5/forecast?q="
+//     .concat(location, "&units=metric&appid=")
+//     .concat(API_KEY);
+
+/**
+ * Gets the weather forecast from the Dark Sky API for the given location.
+ *
+ * @param {Request} req request object from Express.
+ * @param {Response} resp response object from Express.
+ */
+function getForecast(req, resp) {
+  const location = req.params.location;
+  const url = `${BASE_URL}/${API_KEY}/${location}`;
+  fetch(url).then((resp) => {
+    if (resp.status !== 200) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  }).then((data) => {
+    setTimeout(() => {
+      resp.json(data);
+    }, FORECAST_DELAY);
+  }).catch((err) => {
+    console.error('Open weather API Error:', err.message);
+    
+  });
+}
+
+/**
+ * Starts the Express server.
+ *
+ * @return {ExpressServer} instance of the Express server.
+ */
+function startServer() {
+  const app = express();
+
+  // Redirect HTTP to HTTPS,
+  app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+
+  // Logging for each request
+  app.use((req, resp, next) => {
+    const now = new Date();
+    const time = `${now.toLocaleDateString()} - ${now.toLocaleTimeString()}`;
+    const path = `"${req.method} ${req.path}"`;
+    const m = `${req.ip} - ${time} - ${path}`;
+    // eslint-disable-next-line no-console
+    console.log(m);
+    next();
+  });
+
+  // Handle requests for the data
+  app.get('/forecast/:location', getForecast);
+  app.get('/forecast/', getForecast);
+  app.get('/forecast', getForecast);
+
+  // Handle requests for static files
+  app.use(express.static('public'));
+
+  // Start the server
+  return app.listen('8000', () => {
+    // eslint-disable-next-line no-console
+    console.log('Local DevServer Started on port 8000...');
+  });
+}
+
+startServer();
